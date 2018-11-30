d9a60cd964548a837b8fa5e7213f3daa6ea0fbac
Urban Sanden <hej@urre.me>
2018-11-30 10:04:59 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// https://www.robinwieruch.de/react-state-array-add-update-remove/
+import React, { Component } from 'react'
+import tinycolor from 'tinycolor2'
+import './App.css'
+import Footer from './components/Footer'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props)
+
+		this.state = { value: 3, colors: [...Array(48).keys()], hexes: [] }
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(event) {
+		this.setState({ value: parseInt(event.target.value) })
+		this.generateColors()
+	}
+
+	generateColors() {
+		let mon = this.state.value
+		let hexes = this.state.colors.map(function(item) {
+			return tinycolor('#fff')
+				.darken(item + mon)
+				.toString()
+		})
+
+		this.setState({ hexes: hexes })
+	}
+
+	componentWillMount() {
+		this.generateColors()
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<header className="header">
+					<input
+						className="slider"
+						id="typeinp"
+						type="range"
+						min="0"
+						max="50"
+						value={this.state.value}
+						onChange={this.handleChange}
+						step="1"
+					/>
+				</header>
+				<ul>
+					{this.state.hexes.map(item => (
+						<li style={{ backgroundColor: item }} key={item}>
+							<span className="color-name">{item}</span>
+						</li>
+					))}
+				</ul>
+				<Footer />
+			</React.Fragment>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<footer className="footer" role="contentinfo">
+			{' '}
+			<p>
+				By <a href="https://urre.me">Urban Sandén</a>
+			</p>
+		</footer>
+	)
+}
+
+export default Footer
